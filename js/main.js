11ab8817139088a99affda55c9f948aa19a3c59d
rupeshpurum1 <rupeshpurum@usf.edu>
2024-04-21 19:57:34 -0400

    adding the uncommitted changes to mydev branch

--- js/main.js
+++ js/main.js
@@ -5,6 +5,32 @@
 /**
  * Sample code
  */
+
+
+const videoId = 'wU58Go13G6Q'; // Replace with your video ID
+const videoElement = document.getElementById('hero_video');
+
+const player = new YT.Player('hero_video', {
+    videoId: videoId,
+    playerVars: {
+        autoplay: 1,
+        loop: 1,
+        playlist: videoId,
+        mute: 1,
+        controls: 0,
+        showinfo: 0,
+        rel: 0,
+    },
+    events: {
+        'onReady': onPlayerReady
+    }
+});
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+
 document.addEventListener('DOMContentLoaded', function() {
   var ulElement = document.getElementById('links');
   var liElement = document.createElement('li');
